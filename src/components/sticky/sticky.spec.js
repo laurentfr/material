46c953f69146954d63fd2ebdd1e3fbc22f483439
Ryan Schmukler <rschmukler@gmail.com>
2014-09-16 01:47:40 -0400

    test(sticky): add test scenarios. Tests to come

--- src/components/sticky/sticky.spec.js
+++ src/components/sticky/sticky.spec.js
@@ -1,7 +1,8 @@
 describe('$materialStickySpec', function() {
   beforeEach(module('materia.components.sticky'));
   // Todo, how to test this stuff?
-  it('adds material-sticky-active to active element');
-  it('adds material-transition to a transitioning element');
-  it('computes the style of transition elements on scroll');
+  it('adds material-sticky-active when an element would scroll off screen');
+  it('pushes the active element when the next sticky element touches it');
+  it('pulls the active element when the next sticky element losens');
+  it('throws an error if uses outside of material-content');
 });
